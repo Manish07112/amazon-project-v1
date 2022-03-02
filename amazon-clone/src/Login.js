63e084f07c4e31ce82d4ba40ef1c77d87e7a38c7
Manish07112 <manish.yadav07112@gmail.com>
2022-03-02 21:36:49 +0530

    init

--- amazon-clone/src/Login.js
+++ amazon-clone/src/Login.js
@@ -9,7 +9,7 @@ function Login() {
        <img className="login--logo" src="http://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg"alt="amazon"/> 
        </Link>
        <div className='login--container'>
-           <h1>Sign-in</h1>
+           <h1>Sign-In</h1>
        <form>    
            <h5>E-mail</h5>
            <input type='text' />
@@ -19,7 +19,11 @@ function Login() {
            <button className='login--signButton'>Continue</button>
         </form>
         <p>By continuing, you agree to Amazon's Conditions of Use and Privacy Notice.</p>
+        <p1>---------------New To Amazon-------------</p1>
         <button className='login--registerButton'>Create your Amazon Account</button>
+       </div>
+       <div>
+         <h6>© 1996-2022, Amazon.com, Inc. or its affiliates</h6>
        </div>
     </div>
   )
