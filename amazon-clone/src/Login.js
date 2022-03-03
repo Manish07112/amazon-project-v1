230452a5dd3f5e2a988a483370ee0e545104003a
Manish07112 <manish.yadav07112@gmail.com>
2022-03-03 22:01:52 +0530

    init

--- amazon-clone/src/Login.js
+++ amazon-clone/src/Login.js
@@ -1,30 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Login.css';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const signIn = e => {
+    e.preventDefault();
+    //firebase
+  }
+
+  const register = e => {
+    e.preventDefault();
+    //firebase
+  }
+
+
   return (
     <div className='login'>
-       <Link to = "/">
-       <img className="login--logo" src="http://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg"alt="amazon"/> 
-       </Link>
-       <div className='login--container'>
-           <h1>Sign-In</h1>
-       <form>    
-           <h5>E-mail</h5>
-           <input type='text' />
-           <h5>Password</h5>
-           <input type='password' />
+      <Link to="/">
+        <img className="login--logo" src="http://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg" alt="amazon" />
+      </Link>
+      <div className='login--container'>
+        <h1>Sign-In</h1>
+        <form>
+          <h5>E-mail</h5>
+          <input type='text' value={email} onChange={e => setEmail(e.target.value)} />
+          <h5>Password</h5>
+          <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
 
-           <button className='login--signButton'>Continue</button>
+          <button type='submit' onClick={signIn} className='login--signButton'>Continue</button>
         </form>
         <p>By continuing, you agree to Amazon's Conditions of Use and Privacy Notice.</p>
         <p1>--------------New To Amazon?------------</p1>
-        <button className='login--registerButton'>Create your Amazon Account</button>
-       </div>
-       <div>
-         <h6>© 1996-2022, Amazon.com, Inc. or its affiliates</h6>
-       </div>
+        <button type='submit' onClick={register} className='login--registerButton'>Create your Amazon Account</button>
+      </div>
+      <div>
+        <h6>© 1996-2022, Amazon.com, Inc. or its affiliates</h6>
+      </div>
     </div>
   )
 }
--- amazon-clone/src/firebase.js
+++ amazon-clone/src/firebase.js
@@ -1,9 +1,8 @@
-// eslint-disable-next-line no-unused-vars
 const firebaseConfig = {
-    apiKey: "AIzaSyBwIgE-PABI0pAx8wc1C3I1enRTWzIY33I",
-    authDomain: "clone-a2184.firebaseapp.com",
-    projectId: "clone-a2184",
-    storageBucket: "clone-a2184.appspot.com",
-    messagingSenderId: "49025869383",
-    appId: "1:49025869383:web:1d32a1183f7c65ab737362"
+    apiKey: "AIzaSyDQEV_5GRuOTcl5n-u0UDZIHnYJ0VOQtu4",
+    authDomain: "project-3998a.firebaseapp.com",
+    projectId: "project-3998a",
+    storageBucket: "project-3998a.appspot.com",
+    messagingSenderId: "582647952825",
+    appId: "1:582647952825:web:9251b349d061f149d2b268"
   };
